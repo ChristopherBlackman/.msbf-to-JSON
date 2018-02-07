1552de1fe877d6d30fd721b953f1ce0e6123049e
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-02-06 19:59:11 -0500

    update to file, fixed an unicode issue

--- anime_list_encoder.py
+++ anime_list_encoder.py
@@ -4,10 +4,14 @@ import xmltodict
 import requests
 import re
 import untangle
+import editdistance
 
 from pprint import pprint
 from bs4 import BeautifulSoup
+import sys  
 
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 FILE = "something.json"
 
@@ -24,6 +28,53 @@ def get_id(name):
             return manga_id
         except AttributeError:
             print('AttributeError - {}'.format(name))
+
+def get_alternatives(url):
+    alt = None
+    syn = None
+    titles = list()
+
+    m = re.compile('(?<=Synonyms:</span>)[\s\d\w,]+')
+    p = re.compile('(?<=</span>)[\s\d\w,]+')
+
+
+    print url
+    for i in range(5):
+        soup = None
+        try:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text,'html.parser')
+
+            alt =  soup.find_all('h2',text='Alternative Titles')[0].find_next()
+            syn =  soup.find_all('h2',text='Alternative Titles')[0].find_next().parent()
+        except Exception:
+            print 'sss'
+            continue
+            pass
+
+        try:
+            synynomes = map(str.strip,m.findall(str(syn))[0].split(','))
+            titles = list(set(list(synynomes)+titles))
+        except Exception:
+            pass
+
+        try:
+            alterante =  alt.text.replace('English:','').strip()
+            titles = list(set(synynomes+titles))
+        except:
+            pass
+        break
+    return titles
+
+
+
+def w_shingles(word,w):
+    items = []
+    for i in range(0,max(len(word)-w+1,0)):
+        items.append(word[i:i+w])
+
+    return set(items)
+
 def main(): 
 
     p = Parser(FILE)
@@ -33,31 +84,10 @@ def main():
     a.getData()
 
     for item_obj in p.parsedItems():
-        print "*"*30
-        pprint(item_obj)
-        pprint(a.find_closest_match(item_obj['name']))
-        print "*"*30
+        match = a.find_closest_match(item_obj['name'])
+        print match
         
     pass
-
-class AnimeListFactory():
-    def create_list_object(status,name):
-        item = {}
-        item['chapter'] = ''
-        item['volume'] = ''
-        item['status'] = ''
-        item['score'] = ''
-        item['times_reread'] = ''
-        item['date_start'] = ''
-        item['date_finish'] = ''
-        item['priority'] = ''
-        item['enable_discussion'] = ''
-        item['enable_rereading'] = ''
-        item['comments'] = ''
-        item['scan_group'] = ''
-        item['tags'] = ''
-        item['retail_volumes'] = ''
-        return item
  
 class AnimeListManga():
     def __init__(self):
@@ -69,10 +99,13 @@ class AnimeListManga():
         r = requests.get(self.url)
 
         url = re.compile(r"(?<=<loc>)\S+(?=</loc>)")
+
         manga_id = re.compile('(?<=/)\d+(?=/)')
         name = re.compile('(?<=\d/)\S+$')
+
         for item in url.findall(r.content):
             try:
+                #print get_alternatives(item)
                 item_obj = {}
                 item_obj['id'] = manga_id.findall(item)[0]
                 item_obj['name'] = name.findall(item)[0]
@@ -81,24 +114,35 @@ class AnimeListManga():
                 pprint('Invalid Item - {}'.format(item))
                 continue
 
-    def find_closest_match(self,name):
+    def find_closest_match(self,name,n=5,p=.10):
         i = 0
-        current = None
+        obj = {}
         for item in self.data_base:
 
-            set1 = set(name.lower().split('_'))
-            set2 = set(item['name'].lower().split('_'))
+            name2 = item['name'].encode('ascii','ignore')
+
+            #print(type(name2))
+            set1 = w_shingles(name.lower(),n)
+            set2 = w_shingles(name2.lower(),n)
 
             ji = self.compute_jaccard_sim(set1,set2)
-            if ji > i:
+            if ji > max(i,p):
                 i = ji
-                current = item
-        return current
+                obj['name'] = item['name']
+                obj['id'] = item['id']
+                obj['js'] = ji
+                obj['search-key'] = name
+
+
+        return obj
                 
 
     def compute_jaccard_sim(self,set_1, set_2):
         n = len(set_1.intersection(set_2))
-        return (n / float(len(set_1) + len(set_2) - n))
+        r = float(len(set_1) + len(set_2) - n)
+        if r == 0:
+            return 0
+        return (n /r)
 
 
 
@@ -118,3 +162,5 @@ class Parser:
             item['status'] = item_obj[3]
             yield item
 main()
+#print get_alternatives('https://myanimelist.net/manga/2/Berserk')
+#get_alternatives('https://myanimelist.net/anime/8516/Baka_to_Test_to_Shoukanjuu_Ni')
